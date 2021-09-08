70081fcc4ccb417607478ff8234a4986e2de449e
davyengone <davy@hackages.io>
2021-09-08 17:31:14 +0200

    exercise 4: switchTo operator

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,31 @@
  * As we progress in this learning, we will use established rules and good practises in
  * Reactive Programming.
  */
-import {btnStart$, btnCancel$, timerDisplay, millisecondsToStr} from "./utils";
-import "hackjam-banner";
-
-let counter = 0,
-  id: NodeJS.Timeout = null;
-
-btnStart$.subscribe({
-  next() {
-    id = setInterval(() => {
-      timerDisplay.textContent = millisecondsToStr(counter++);
-    }, 1000);
-  },
-  complete() {
-    console.log("completed");
-  },
-});
-
-btnCancel$.subscribe({
-  next() {
-    clearInterval(id);
-  },
-});
+ import { interval } from "./lib/interval";
+ import { switchTo } from "./lib/switchTo";
+ import { ISubscription } from "./types";
+ import {
+   btnStart$,
+   btnCancel$,
+   timerDisplay,
+   millisecondsToStr,
+ } from "./utils";
+ import "hackjam-banner";
+ 
+ let subscription: ISubscription = switchTo(btnStart$, interval(1000)).subscribe(
+   {
+     next(period) {
+       timerDisplay.textContent = millisecondsToStr(period);
+     },
+     complete() {
+       console.log("completed");
+     },
+   }
+ );
+ 
+ btnCancel$.subscribe({
+   next() {
+     subscription.unsubscribe();
+   },
+ });
+ 
